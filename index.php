ae409ae7fa9ea0780b61a9eb5b9830b346464f9c
monsieurmagz <monsieurmagz@gmail.com>
2017-03-12 19:00:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Draffles</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="css/mdb.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel='shortcut icon' type='image/x-icon' href='pepe.ico' />
+        <!-- JavaScript -->
+<script src="//cdn.jsdelivr.net/alertifyjs/1.9.0/alertify.min.js"></script>
+
+<!-- CSS -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/alertify.min.css"/>
+<!-- Default theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/themes/default.min.css"/>
+<!-- Semantic UI theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/themes/semantic.min.css"/>
+<!-- Bootstrap theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/themes/bootstrap.min.css"/>
+
+<!-- 
+    RTL version
+-->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/alertify.rtl.min.css"/>
+<!-- Default theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/themes/default.rtl.min.css"/>
+<!-- Semantic UI theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/themes/semantic.rtl.min.css"/>
+<!-- Bootstrap theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.9.0/css/themes/bootstrap.rtl.min.css"/>
+
+    </head>
+    <body>
+     <?php
+     include('codes/registration.php');
+     include('codes/connection.php');
+     echo $invalemail;
+     echo $regsuc;
+     echo $regerr;
+     
+     ?>
+        
+        
+    
+    
+        
+<!--Navigation & Intro-->
+
+<!--Navigation & Intro-->
+<header>
+    
+    <!--Navbar-->
+    <nav class="navbar navbar-toggleable-md navbar-dark fixed-top scrolling-navbar">
+        <div class="container">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav1" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand waves-effect waves-light" href="index.php" >Draffles</a>
+            <div class="collapse navbar-collapse" id="navbarNav1">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="files/login.php">Login</a>
+                    </li>
+                    
+                </ul>
+                
+            </div>
+        </div>
+    </nav>
+    <!--/.Navbar-->
+
+    <!--Mask-->
+    <div class="view hm-black-strong">
+        <div class="full-bg-img flex-center">
+            <div class="container">
+                <div class="row" id="home">
+
+                    <!--First column-->
+                    <div class="col-lg-6">
+                        <div class="description">
+                            <h2 class="h2-responsive wow fadeInLeft">Sign up</h2>
+                            <hr class="hr-dark wow fadeInLeft">
+                            <p class="wow fadeInLeft" data-wow-delay="0.4s">Draffles Practice Website</p>
+                            <br>
+                           
+                        </div>
+                    </div>
+                    <!--/.First column-->
+
+                    <!--Second column-->
+                    <div class="col-lg-6">
+                        <!--Form-->
+                        <div class="card wow fadeInRight">
+                            <div class="card-block">
+                                <!--Header-->
+                                <div class="text-center">
+                                    <h3><i class="fa fa-user"></i> Register:</h3>
+                                    <hr>
+                                </div>
+                                <form method = "post" action = "">
+                                <!--Body-->
+                                <div class="md-form">
+                                    <i class="fa fa-user prefix"></i>
+                                    <input type="text" id="form3" class="form-control" name="username">
+                                    <label for="form3">Username</label>
+                                </div>
+                                <div class="md-form">
+                                    <i class="fa fa-envelope prefix"></i>
+                                    <input type="text" id="form2" class="form-control" name="email">
+                                    <label for="form2">Email</label>
+                                </div>
+
+                                <div class="md-form">
+                                    <i class="fa fa-lock prefix"></i>
+                                    <input type="password" id="form4" class="form-control" name="password">
+                                    <label for="form4">Password</label>
+                                </div>
+                                <div class="text-center">
+                                    <button class="btn btn-ins btn-lg" name="submit"  type="submit">Sign up</button>                                                                     
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!--/.Form-->
+                    </div>
+                    <!--/Second column-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/.Mask-->
+
+</header>
+<!--/Navigation & Intro-->
+
+<!--/Navigation & Intro-->
+     <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+
+    <!-- Tooltips -->
+    <script type="text/javascript" src="js/tether.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="js/alertify.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    </body>
+</html>
