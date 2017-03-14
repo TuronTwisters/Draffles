f65b02e4d94a6bc91867004ee7e3ba123214fdbc
monsieurmagz <monsieurmagz@gmail.com>
2017-03-15 05:43:19 +0800

    Username Email Duplicate check

--- registration.php
+++ registration.php
@@ -38,10 +38,8 @@ if(isset($_POST['submit'])){
     if(preg_replace("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^","", $email)){
     $invalemail = "<script>alertify.alert('Error','Invalid Email Format', function(){alertify.error('Error!');});</script>";
     }else{
-        //Start of username validations
+     //Start of username validations
     $querusername = mysqli_query($conn,"Select username from users where username='$username'");
-    $queremail = mysqli_query($conn, "Select email from users where email ='$email'");
-    $emailrows = mysqli_num_rows($queremail);
     $userrows = mysqli_num_rows($querusername);
     if($userrows > 0){
          $userexist = "<script>alertify.alert('Error','Username had been used', function(){alertify.error('Error!');});</script>";
